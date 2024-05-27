1adecabbda0be55e61737fb58cc4dfb78fdea276
Pyry Sangi <pyry.sangi@helsinki.fi>
2024-05-27 11:24:12 +0300

    Add spatial index to zones table

--- create_database.sh
+++ create_database.sh
@@ -4,7 +4,12 @@ host=$2
 port=$3
 input=$4
 
-create_sql="CREATE TABLE zones (id SERIAL PRIMARY KEY, geom GEOMETRY(POLYGON, 3857));"
+create_sql="
+CREATE TABLE zones (
+	id SERIAL PRIMARY KEY, geom GEOMETRY(POLYGON, 3857)
+);
+CREATE INDEX sidx_zones_geom ON zones USING GIST(geom);
+"
 
 osm2pgsql --slim -H "$host" -P "$port" -U "$user" "$input"
 psql -h "$host" -p "$port" -U "$user" -c "$create_sql"
