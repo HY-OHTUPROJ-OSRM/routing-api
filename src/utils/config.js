03a7a62a264a38506458cd1d2732e59779945b93
fallmx <31561277+fallmx@users.noreply.github.com>
2024-05-30 15:14:45 +0300

    Fix anomaly

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,5 @@
 const PORT = process.env.PORT || 3000
 const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:5000"
-const SRID = process.env.SRID || "3857"
 const DATABASE_HOST = process.env.DATABASE_HOST || ""
 const DATABASE_PORT = process.env.DATABASE_PORT || ""
 const DATABASE_DB = process.env.DATABASE_DB || ""
@@ -11,7 +10,6 @@ const SRID = process.env.SRID || "3857"
 module.exports = {
     PORT,
     BACKEND_URL,
-    SRID,
     DATABASE_HOST,
     DATABASE_PORT,
     DATABASE_DB,
