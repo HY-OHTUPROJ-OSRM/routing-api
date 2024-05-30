0a9ad4318854ea485c7b0cfdb5110203187d5fd3
Pyry Sangi <pyry.sangi@helsinki.fi>
2024-05-30 10:53:25 +0300

    Add SRID to config

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,9 +3,11 @@ const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:5000"
 const DATABASE_USER = process.env.DATABASE_USER || ""
 const DATABASE_HOST = process.env.DATABASE_HOST || ""
 const DATABASE_PORT = process.env.DATABASE_PORT || ""
+const SRID = process.env.SRID || "3857"
 
 module.exports = {
     PORT,
     BACKEND_URL,
-    DATABASE_USER, DATABASE_HOST, DATABASE_PORT
+    DATABASE_USER, DATABASE_HOST, DATABASE_PORT,
+    SRID
 }
