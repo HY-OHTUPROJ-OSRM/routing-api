2a56bc845fee771f0fa0de2c4267ed8fbcf47bdb
Pyry Sangi <pyry.sangi@helsinki.fi>
2024-05-31 13:03:18 +0300

    Start backend using shared memory

--- start_backend.sh
+++ start_backend.sh
@@ -1,3 +1,4 @@
-osrm-extract -p /opt/car.lua $1
-osrm-contract $1
-osrm-routed --algorithm ch $1 & disown
+osrm-extract -p /opt/car.lua "$1"
+osrm-contract "$1"
+osrm-datastore "$1"
+osrm-routed --shared-memory --algorithm ch & disown
