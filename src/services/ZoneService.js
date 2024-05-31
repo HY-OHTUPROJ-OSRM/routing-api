c9511f0a8d78b8677f61fe3db9ab520c8e2d6d5a
Pyry Sangi <pyry.sangi@helsinki.fi>
2024-05-31 15:10:30 +0300

    Buffer intersections program input in smaller chunks

--- src/services/ZoneService.js
+++ src/services/ZoneService.js
@@ -6,6 +6,34 @@ const validator = require("../components/Validators")
 
 const blockedSegments = new Set()
 
+function binaryWriter(stream) {
+    const bufferSize = 512
+    const buffer = Buffer.alloc(bufferSize)
+    let numBytes = 0
+
+    const flushIfFull = () => {
+        if (numBytes >= bufferSize) {
+            stream.write(buffer)
+            numBytes = 0
+        }
+    }
+
+    return {
+        writeNumber: (n) => {
+            numBytes = buffer.writeBigUInt64LE(BigInt(n), numBytes)
+            flushIfFull()
+        },
+        writeVertex: (x, y) => {
+            numBytes = buffer.writeInt32LE(x, numBytes)
+            numBytes = buffer.writeInt32LE(y, numBytes)
+            flushIfFull()
+        },
+        writeEnd: () => {
+            stream.end(buffer.subarray(0, numBytes))
+        }
+    }
+}
+
 class ZoneService {
     static async getZones() {
         const zones = await ZoneRepository.getZones()
@@ -33,33 +61,33 @@ class ZoneService {
      *                 segments. */
     static async waysOverlappingZone(zoneIds, zoneGeometries) {
         const paths = await ZoneRepository.getOverlappingPaths(zoneIds)
+        const child = spawn("Polygonal-Intersections-CLI")
+        const { writeNumber, writeVertex, writeEnd } = binaryWriter(child.stdin)
+        const node_pairs = []
+        const result = new Promise((resolve, reject) => {
+            child.stdout.on("end", () => {
+                resolve(node_pairs)
+            })
+        })
 
-        const inputSize = 8 * (2
-                + zoneGeometries.length + zoneGeometries.flat().length
-                + paths.length + 2 * paths.flat().length)
-        const input = Buffer.alloc(inputSize)
-        let offset = 0
+        child.stdout.on("readable", () => {
+            for (let to_read = child.stdout.readableLength; to_read >= 16; to_read -= 16) {
+                const pair = [0, 0]
+                pair[0] = child.stdout.read(8).readInt32LE()
+                pair[1] = child.stdout.read(8).readInt32LE()
+                node_pairs.push(pair)
+            }
+        })
 
-        const writeCount = (n) => {
-            offset = input.writeBigInt64LE(BigInt(n), offset)
-        }
-
-        const writeNodeId = writeCount
-
-        const writeVertex = (x, y) => {
-            offset = input.writeInt32LE(x, offset)
-            offset = input.writeInt32LE(y, offset)
-        }
-
-        /* Construct intersection program input.
+        /* Write intersection program input.
          * https://github.com/HY-OHTUPROJ-OSRM/osrm-project/wiki/Intersection-Algorithm */
 
-        writeCount(zoneGeometries.length)
-        writeCount(paths.length)
+        writeNumber(zoneGeometries.length)
+        writeNumber(paths.length)
 
-        /* Polygons */
+        /* Polygons. */
         for (const polygon of zoneGeometries) {
-            writeCount(polygon.length)
+            writeNumber(polygon.length)
 
             for (const vert of polygon) {
                 writeVertex(vert[0] * 10000000, vert[1] * 10000000)
@@ -68,36 +96,20 @@ class ZoneService {
 
         /* Paths. */
         for (const path of paths) {
-            writeCount(path.length)
+            writeNumber(path.length)
 
             for (const vert of path) {
                 writeVertex(vert.lat, vert.lon)
             }
 
             for (const vert of path) {
-                writeNodeId(vert.id)
+                writeNumber(vert.id)
             }
         }
 
-        return new Promise((resolve, reject) => {
-            const child = spawn("Polygonal-Intersections-CLI")
-            const node_pairs = []
+        writeEnd()
 
-            child.stdout.on("readable", () => {
-                for (let to_read = child.stdout.readableLength; to_read >= 16; to_read -= 16) {
-                    const pair = [0, 0]
-                    pair[0] = child.stdout.read(8).readInt32LE()
-                    pair[1] = child.stdout.read(8).readInt32LE()
-                    node_pairs.push(pair)
-                }
-            })
-
-            child.stdout.on("end", () => {
-                resolve(node_pairs)
-            })
-
-            child.stdin.end(input)
-        })
+        return result
     }
 
     static async blockSegments(segments) {
