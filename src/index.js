211e0fe8b4dd454e77c8b8fa96862bf2cde10ad7
pants64DS <pants64DS@users.noreply.github.com>
2024-06-03 10:22:07 +0300

    Update .gitignore, make things compatible with WSL

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ const server = require("./server")
 const { execSync, exec } = require('child_process');
 
 try {
-    console.log(execSync(`./create_database.sh ${DATABASE_USER} ${DATABASE_PASSWORD} ${DATABASE_HOST} ${DATABASE_PORT} ./route-data.osm`))
-} catch {}
-
-exec("./start_backend.sh ./route-data.osm")
+    console.log(execSync("./create_database.sh", { encoding: 'utf-8' }))
+    exec("./start_backend.sh")
+}
+catch(error)
+{
+    console.log("Exception!!!")
+    console.log(error.message)
+}
 
 server.listen(PORT, () => {
     console.log(`routing-api listening on port ${PORT}`)
--- start_backend.sh
+++ start_backend.sh
@@ -1,4 +1,4 @@
-osrm-extract -p /opt/car.lua "$1"
-osrm-contract "$1"
-osrm-datastore "$1"
+osrm-extract -p /opt/car.lua "./route-data.osm"
+osrm-contract "./route-data.osm"
+osrm-datastore "./route-data.osm"
 osrm-routed --shared-memory --algorithm ch & disown
