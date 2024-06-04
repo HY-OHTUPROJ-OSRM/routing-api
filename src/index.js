9b09e7affa07a8a8697d26281609dc14a41829ea
pants64DS <pants64DS@users.noreply.github.com>
2024-06-05 02:16:58 +0300

    Wait for osrm-routed to fully start before starting the server

--- src/index.js
+++ src/index.js
@@ -3,24 +3,12 @@ const { PORT, DATABASE_USER, DATABASE_PASSWORD, DATABASE_HOST, DATABASE_PORT } =
 const server = require("./server")
 const { spawn } = require('child_process');
 const ZoneService = require("./services/ZoneService")
-const { execSyncCustom, makeOutputReader } = require("./utils/process_utils")
+const { formatOutput, execSyncCustom, makeOutputReader } = require("./utils/process_utils")
 
 execSyncCustom("create_database.sh", "./create_database.sh")
 execSyncCustom("osrm-extract",   "osrm-extract -p /opt/car.lua ./route-data.osm")
 execSyncCustom("osrm-contract",  "osrm-contract ./route-data.osm")
 execSyncCustom("osrm-datastore", "osrm-datastore ./route-data.osm")
-
-const osrm = spawn("osrm-routed", ["--shared-memory", "--algorithm", "ch"])
-
-osrm.stdout.on("data", makeOutputReader("osrm-routed", process.stdout))
-osrm.stderr.on("data", makeOutputReader("osrm-routed", process.stderr))
-
-osrm.on("exit", (code, signal) => {
-    if (code != 0) {
-        console.error("osrm-routed has faulted!")
-        process.exit()
-    }
-})
 
 const startServer = async () => {
     const segments = await ZoneService.waysOverlappingAnyZone()
@@ -37,4 +25,23 @@ const startServer = async () => {
     })
 }
 
-startServer()
+const osrm = spawn("osrm-routed", ["--shared-memory", "--algorithm", "ch"])
+var started = false;
+
+osrm.stdout.on("data", (output) => {
+    process.stdout.write(formatOutput("osrm-routed", output))
+
+    if (!started && output.toString().includes("running and waiting for requests")) {
+        startServer()
+        started = true
+    }
+})
+
+osrm.stderr.on("data", makeOutputReader("osrm-routed", process.stderr))
+
+osrm.on("exit", (code, signal) => {
+    if (code != 0) {
+        console.error("osrm-routed has faulted!")
+        process.exit()
+    }
+})
