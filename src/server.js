f8cb1c773442f349497aa8b1dad499f7659b8b46
Juho Turunen <juzturun@cubbli24-gold.cs.helsinki.fi>
2025-05-21 14:20:56 +0300

    Connect new temp_road code to rest of project

--- src/server.js
+++ src/server.js
@@ -1,26 +1,34 @@
-const proxy = require('express-http-proxy')
-const express = require("express")
-const cors = require("cors")
+const proxy = require("express-http-proxy");
+const express = require("express");
+const cors = require("cors");
 
-const { BACKEND_URL } = require('./utils/config')
+const { BACKEND_URL } = require("./utils/config");
 
-const zoneRouter = require("./routes/zones")
-const segmentRouter = require("./routes/segments")
-const statusRouter = require("./routes/status")
+const zoneRouter = require("./routes/zones");
+const segmentRouter = require("./routes/segments");
+const statusRouter = require("./routes/status");
+const tempRouter = require("./routes/temps");
 
-const server = express()
+const server = express();
 
 server.use(express.json());
-server.use(cors())
+server.use(cors());
 
-server.use("/route", proxy(`${BACKEND_URL}/route`, {
-    proxyReqPathResolver: (req) => `/route${req.url}`
-}))
-server.use("/tile", proxy(`${BACKEND_URL}/tile`, {
-    proxyReqPathResolver: (req) => `/tile${req.url}`
-}))
-server.use("/zones", zoneRouter)
-server.use("/segments", segmentRouter)
-server.use("/status", statusRouter)
+server.use(
+  "/route",
+  proxy(`${BACKEND_URL}/route`, {
+    proxyReqPathResolver: (req) => `/route${req.url}`,
+  })
+);
+server.use(
+  "/tile",
+  proxy(`${BACKEND_URL}/tile`, {
+    proxyReqPathResolver: (req) => `/tile${req.url}`,
+  })
+);
+server.use("/zones", zoneRouter);
+server.use("/segments", segmentRouter);
+server.use("/status", statusRouter);
+server.use("/temps", tempRouter);
 
-module.exports = server
+module.exports = server;
