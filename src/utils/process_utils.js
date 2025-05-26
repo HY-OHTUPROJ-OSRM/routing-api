30c592808b00679ea2a881f0808986c4283877e7
Juho Turunen <juho.turunen89@gmail.com>
2025-05-26 19:46:44 +0300

    Refactor utils

--- src/utils/process_utils.js
+++ src/utils/process_utils.js
@@ -1,27 +1,27 @@
-const { execSync } = require('child_process');
-const { progress } = require('../services/StatusService');
+const { execSync } = require("child_process");
+const { progress } = require("../services/StatusService");
 
 function formatOutput(name, output) {
-    return `\n[${name}] ${output.toString().split('\n').join(`\n[${name}] `)}`
+  return `\n[${name}] ${output.toString().split("\n").join(`\n[${name}] `)}`;
 }
 
 function makeOutputReader(name, dest) {
-    return (output) => {
-        dest.write(formatOutput(name, output))
-        
-        if (output.includes("100%")) {
-            progress()
-        }
+  return (output) => {
+    dest.write(formatOutput(name, output));
+
+    if (output.includes("100%")) {
+      progress();
     }
+  };
 }
 
 function execSyncCustom(name, command) {
-    try {
-        console.log(formatOutput(name, execSync(command, { encoding: 'utf-8' })))
-    } catch(error) {
-        console.error(`Error in ${name}:\n${error}`)
-        process.exit()
-    }
+  try {
+    console.log(formatOutput(name, execSync(command, { encoding: "utf-8" })));
+  } catch (error) {
+    console.error(`Error in ${name}:\n${error}`);
+    process.exit();
+  }
 }
 
-module.exports = {formatOutput, makeOutputReader, execSyncCustom}
+module.exports = { formatOutput, makeOutputReader, execSyncCustom };
