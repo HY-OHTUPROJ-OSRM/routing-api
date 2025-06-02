90f2e72a19a36710f33b9f138772bbee79aef393
Juho Turunen <juzturun@cubbli24-gold.cs.helsinki.fi>
2025-06-02 15:11:23 +0300

    Switch to permanent profile class names

--- profiles/vehicle_class_config.lua
+++ profiles/vehicle_class_config.lua
@@ -2,14 +2,14 @@
 return {
     -- Weight is in kilograms
     weight_classes = {
-        { name = "under10k", cutoff = 10000 },
-        { name = "under20k", cutoff = 20000 },
-        { name = "under30k", cutoff = 30000 }
+        { name = "weightLow", cutoff = 10000 },
+        { name = "weightMedium", cutoff = 20000 },
+        { name = "weightHigh", cutoff = 30000 }
     },
     -- Height is in meters
     height_classes = {
-        { name = "under3m", cutoff = 3.0 },
-        { name = "under4m", cutoff = 4.0 },
-        { name = "under5m", cutoff = 5.0 }
+        { name = "heightLow", cutoff = 3.0 },
+        { name = "heightMedium", cutoff = 4.0 },
+        { name = "heightHigh", cutoff = 5.0 }
     }
 }
