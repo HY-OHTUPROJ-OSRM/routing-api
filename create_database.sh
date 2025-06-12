23fd0e420c75592ada96e60f2857bdf640315240
aatufutaa <103244180+aatufutaa@users.noreply.github.com>
2025-06-12 16:33:56 +0300

    fix slow api

--- create_database.sh
+++ create_database.sh
@@ -69,14 +69,33 @@ CREATE INDEX IF NOT EXISTS idx_temp_routes_end_node ON temporary_routes(end_node
 CREATE INDEX IF NOT EXISTS sidx_zones_geom ON zones USING GIST(geom);
 "
 
-PGPASSWORD=$DATABASE_PASSWORD osm2pgsql --slim -H "$DATABASE_HOST" -P "$DATABASE_PORT" -U "$DATABASE_USER" "${ROUTE_DATA_PATH:-./map_data/route-data.osm}"
+# Check if a table exists
+table_exists=$(PGPASSWORD=$DATABASE_PASSWORD psql -h "$DATABASE_HOST" -p "$DATABASE_PORT" -U "$DATABASE_USER" -t -c \
+"SELECT to_regclass('public.planet_osm_line');" | tr -d '[:space:]')
+
+# If the table doesn't exist, run osm2pgsql
+if [ "$table_exists" = "" ] || [ "$table_exists" = "null" ]; then
+    echo "Creating database schema..."
+    PGPASSWORD=$DATABASE_PASSWORD osm2pgsql --slim -H "$DATABASE_HOST" -P "$DATABASE_PORT" -U "$DATABASE_USER" \
+        "${ROUTE_DATA_PATH:-./map_data/route-data.osm}"
+else
+    echo "Database schema already exists. Skipping osm2pgsql."
+fi
+echo "Creating tables and indexes..."
 PGPASSWORD=$DATABASE_PASSWORD psql -h $DATABASE_HOST -p "$DATABASE_PORT" -U "$DATABASE_USER" -c "$create_sql"
 
-json=$(curl -s "https://data.stat.fi/api/classifications/v2/classifications/kunta_1_20250101/classificationItems?content=data&meta=max&lang=fi&format=json")
+echo  "Checking if municipalities table is empty..."
+row_count=$(PGPASSWORD=$DATABASE_PASSWORD psql -h "$DATABASE_HOST" -p "$DATABASE_PORT" -U "$DATABASE_USER" -t -c "SELECT COUNT(*) FROM municipalities;" | tr -d '[:space:]')
+echo "Row count in municipalities table: $row_count"
+if [ "$row_count" -eq 0 ]; then
+    echo "Inserting municipalities data into the database..."
+    json=$(curl -s "https://data.stat.fi/api/classifications/v2/classifications/kunta_1_20250101/classificationItems?content=data&meta=max&lang=fi&format=json")
 
-echo "$json" | jq -c '.[] | {code: .code, name: .classificationItemNames[0].name}' | while read -r item; do
-    code=$(echo "$item" | jq -r '.code')
-    name=$(echo "$item" | jq -r '.name' | sed "s/'/''/g")
-    PGPASSWORD=$DATABASE_PASSWORD psql -h $DATABASE_HOST -p "$DATABASE_PORT" -U "$DATABASE_USER" -c "INSERT INTO municipalities (code, name) VALUES ('$code', '$name') ON CONFLICT (code) DO NOTHING;"
-done
-
+    echo "$json" | jq -c '.[] | {code: .code, name: .classificationItemNames[0].name}' | while read -r item; do
+        code=$(echo "$item" | jq -r '.code')
+        name=$(echo "$item" | jq -r '.name' | sed "s/'/''/g")
+        PGPASSWORD=$DATABASE_PASSWORD psql -h "$DATABASE_HOST" -p "$DATABASE_PORT" -U "$DATABASE_USER" \
+            -c "INSERT INTO municipalities (code, name) VALUES ('$code', '$name') ON CONFLICT (code) DO NOTHING;" \
+            >/dev/null 2>&1
+    done
+fi
